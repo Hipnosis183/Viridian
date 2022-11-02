e184afd5db9a2affe15035059c117c1d1ce6daad
Renzo Pigliacampo <renzopigliacampo@gmail.com>
2022-11-02 03:31:19 -0300

    Added modal UI component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// App components.
 import { VideoPlayerComponent } from './components/video-player/video-player.component';
+
+// UI components.
+import { ModalComponent } from './ui/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VideoPlayerComponent
+    VideoPlayerComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ui/modal/modal.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+
+export class ModalComponent { }
