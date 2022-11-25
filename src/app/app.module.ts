09ca2722c7ab0de4717e0a5442809a42b0b8f12e
Renzo Pigliacampo <renzopigliacampo@gmail.com>
2022-11-25 15:23:59 -0300

    Implemented translate/i18n library, reorganised strings for its use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,6 +29,22 @@ import { DurationPipe } from './pipes/duration.pipe';
 import { ExpressionPipe } from './pipes/expression.pipe';
 import { FileSizePipe } from './pipes/filesize.pipe';
 
+// Translate service setup.
+import { Injector, APP_INITIALIZER } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+function appInitializerFactory(translate: TranslateService) {
+  return () => {
+    translate.addLangs(['en']);
+    translate.setDefaultLang('en');
+    return translate.use('en').toPromise();
+  };
+}
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -47,8 +66,20 @@ import { FileSizePipe } from './pipes/filesize.pipe';
     AppRoutingModule,
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
-  providers: [],
+  providers: [{
+    provide: APP_INITIALIZER, multi: true,
+    useFactory: appInitializerFactory,
+    deps: [TranslateService, Injector]
+  }],
   bootstrap: [AppComponent]
 })
 
