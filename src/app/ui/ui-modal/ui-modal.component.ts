fa0aa3d5458e25af36e2541c0e3644b4f25e9b83
Renzo Pigliacampo <renzopigliacampo@gmail.com>
2022-12-10 13:25:14 -0300

    ModalUI: Added hidden attribute

--- src/app/ui/ui-modal/ui-modal.component.ts
+++ src/app/ui/ui-modal/ui-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ui-modal',
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./ui-modal.component.css']
 })
 
-export class UiModalComponent { }
+export class UiModalComponent {
+  
+  @Input() modalHidden: boolean = false;
+}
