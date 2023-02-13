b8b9878c53d3f33f411aa7c55ba0cf931761eaed
Renzo Pigliacampo <renzopigliacampo@gmail.com>
2023-02-13 18:15:45 -0300

    Added video time and duration display

--- src/app/pipes/duration.pipe.ts
+++ src/app/pipes/duration.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'duration' })
 export class DurationPipe implements PipeTransform {
 
-  transform(seconds: number): string {
-    return new Date(seconds * 1000).toISOString().substring(11, 19);
+  transform(seconds: number, ms: boolean = false): string {
+    return new Date(seconds * 1000).toISOString().substring(11, (ms ? 23 : 19));
   }
 }
